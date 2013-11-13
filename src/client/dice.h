50d886d81d126da71734936e3cac4d573b8fca88
Bums Kim <bskim45@gmail.com>
2013-11-14 01:35:52 +0900

    Dice: applied singletone

--- src/client/dice.h
+++ src/client/dice.h
@@ -1,17 +1,22 @@
 #pragma once
 
-#include<ctime>
-#include<cstdlib>
-
 class Dice
 {
-protected:
+private:
+	Dice();
+	static Dice* m_inst;
 	int value1;
 	int value2;
+
 public:
+	~Dice();
 	void roll();
 	bool isDouble();
 	int getSum();
+
+	// Static Methods
+	static Dice* getInst();
+	static void delInst();
 };
 
 
