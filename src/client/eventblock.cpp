34a8300092dddb09eaefcf854b1233604b91412e
HamG <tedkhg@gmail.com>
2013-11-14 22:06:24 +0900

    event block : set basic functions

--- src/client/eventblock.cpp
+++ src/client/eventblock.cpp
@@ -1,5 +1,40 @@
 #include "eventblock.h"
+#include <iostream>
+
+using namespace std;
 
 EventBlock::EventBlock()
+{
+}
+
+EventBlock::~EventBlock()
+{
+}
+
+void EventBlock::drink(Player* player)
+{
+}
+
+void EventBlock::cc(Player* player)
+{
+}
+
+void EventBlock::takeSubject(Player* player)
+{
+}
+
+void EventBlock::loseSubject(Player* player)
+{
+}
+
+void EventBlock::lol(Player* player)
+{
+}
+
+void EventBlock::eatChicken(Player* player)
+{
+}
+
+void EventBlock::clubActivity(Player* player)
 {
 }
--- src/client/eventblock.h
+++ src/client/eventblock.h
@@ -5,6 +5,15 @@ class EventBlock : public Block
 {
 public:
     EventBlock();
+	~EventBlock();
+
+	void drink(Player* player);
+	void cc(Player* player);
+	void takeSubject(Player* player);
+	void loseSubject(Player* player);
+	void lol(Player* player);
+	void eatChicken(Player* player);
+	void clubActivity(Player* player);
 };
 
 #endif // EVENTBLOCK_H
