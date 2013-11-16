fc9fe851f132afed353dacbc7dec41be28812709
Bums Kim <bskim45@gmail.com>
2013-11-16 23:18:53 +0900

    Block: several changes enum 61CALL->CALLTAXI, added some methods, solved circular dependency

--- src/client/block.h
+++ src/client/block.h
@@ -1,19 +1,23 @@
-#ifndef BLOCK_H
-#define BLOCK_H
+#pragma once
+#include "block.h"
+
+// circular dependency
+class Board;
+class Player;
 
 class Block
 {
 public:
+    enum CornerType
+    {
+        DORMITORY,
+        CALLTAXI,
+        BREAKSEMESTER,
+        PLURALMAJOR
+    };
+public:
     Block();
+    virtual ~Block();
+    virtual void enter(Player* player);
 };
 
-enum CornerType
-{
-	DORMITORY,
-	61CALL,
-	BREAKSEMESTER,
-	PLURALMAJOR
-};
-
-
-#endif // BLOCK_H
