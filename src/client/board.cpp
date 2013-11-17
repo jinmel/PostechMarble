61b21adaed5d791e6073308e2ace837ba9e3eeef
Wonpyo-Park <parkwonpyo@postech.ac.kr>
2013-11-17 22:06:42 +0900

    Wonpyo_boards_change

--- src/client/board.cpp
+++ src/client/board.cpp
@@ -1,8 +1,67 @@
 #include "board.h"
+#include "eventblock.h"
+#include "subjectblock.h"
+#include "cornerblock.h"
+
 
 Board::Board()
 {
     boards = new Block*[36];
+	boards[0] = new CornerBlock;
+	boards[1] = new SubjectBlock;
+	boards[2] = new SUbjectBlock;
+	boards[3] = new SUbjectBlock;
+	
+	boards[4] = new SubjectBlock;
+	?
+	boards[6] = new SubjectBlock;
+	boards[7] = new SubjectBlock;
+
+	boards[8] = new EventBlock;
+	
+	boards[9] = new CornerBlock;
+
+	boards[10] = new SubjectBlock;
+	boards[11] = new SubjectBlock;
+	boards[12] = new SubjectBlock;
+
+	boards[13] = new EventBlock;
+	
+	boards[14] = new SubjectBlock;
+	boards[15] = new SubjectBlock;
+	boards[16] = new SubjectBlock;
+	
+	?
+
+	boards[18] = new CornerBlock;
+	
+	boards[19] = new SubjectBlock;
+	boards[20] = new SubjectBlock;
+	boards[21] = new SubjectBlock;
+
+	boards[22] = new EventBlock;
+
+	boards[23] = new SubjectBlock;
+	boards[24] = new SubjectBlock;
+	boards[25] = new SubjectBlock;
+	
+	?
+
+	boards[27] = new CornerBlock;
+	
+	?
+
+	boards[29] = new SubjectBlock;
+	boards[30] = new SubjectBlock;
+	boards[31] = new SubjectBlock;
+	
+	boards[32] = new EventBlock;
+
+	boards[33] = new SubjectBlock;
+	boards[34] = new SubjectBlock;
+	boards[35] = new SubjectBlock;
+	
+
 }
 
 
