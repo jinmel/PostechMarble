4ff44569bd0554264ae5822a42a984b1f1b65910
Bums Kim <bskim45@gmail.com>
2013-11-17 22:56:33 +0900

    Fix board

--- src/client/board.cpp
+++ src/client/board.cpp
@@ -11,60 +11,58 @@ Board::Board()
 
 	boards[0] = new CornerBlock;
 
+	boards[1] = new SubjectBlock;
+	boards[2] = new SUbjectBlock;
+	boards[3] = new SUbjectBlock;
 
-	static_cast<SubjectBlock*>boards[1] = new SubjectBlock;
-	static_cast<SubjectBlock*>boards[2] = new SUbjectBlock;
-	static_cast<SubjectBlock*>boards[3] = new SUbjectBlock;
 
-
-	static_cast<SubjectBlock*>boards[4] = new SubjectBlock;
-	//°ü±¤Áö
-	static_cast<SubjectBlock*>boards[6] = new SubjectBlock;
-	static_cast<SubjectBlock*>boards[7] = new SubjectBlock;
+	boards[4] = new SubjectBlock;
+	// ê´€ê´‘ì§€
+	boards[6] = new SubjectBlock;
+	boards[7] = new SubjectBlock;
 
 	boards[8] = new EventBlock;
 	
 	boards[9] = new CornerBlock;
 
-	static_cast<SubjectBlock*>boards[10] = new SubjectBlock;
-	static_cast<SubjectBlock*>boards[11] = new SubjectBlock;
-	static_cast<SubjectBlock*>boards[12] = new SubjectBlock;
+	boards[10] = new SubjectBlock;
+	boards[11] = new SubjectBlock;
+	boards[12] = new SubjectBlock;
 
 	boards[13] = new EventBlock;
 	
-	static_cast<SubjectBlock*>boards[14] = new SubjectBlock;
-	static_cast<SubjectBlock*>boards[15] = new SubjectBlock;
-	static_cast<SubjectBlock*>boards[16] = new SubjectBlock;
+	boards[14] = new SubjectBlock;
+	boards[15] = new SubjectBlock;
+	boards[16] = new SubjectBlock;
 	
-	//°ü±¤Áö
-
+	// ê´€ê´‘ì§€
 	boards[18] = new CornerBlock;
 	
-	static_cast<SubjectBlock*>boards[19] = new SubjectBlock;
-	static_cast<SubjectBlock*>boards[20] = new SubjectBlock;
-	static_cast<SubjectBlock*>boards[21] = new SubjectBlock;
+	boards[19] = new SubjectBlock;
+	boards[20] = new SubjectBlock;
+	boards[21] = new SubjectBlock;
 
 	boards[22] = new EventBlock;
 
-	static_cast<SubjectBlock*>boards[23] = new SubjectBlock;
-	static_cast<SubjectBlock*>boards[24] = new SubjectBlock;
-	static_cast<SubjectBlock*>boards[25] = new SubjectBlock;
+	boards[23] = new SubjectBlock;
+	boards[24] = new SubjectBlock;
+	boards[25] = new SubjectBlock;
 	
-	//°ü±¤Áö
+	// ê´€ê´‘ì§€
 
 	boards[27] = new CornerBlock;
 	
-	//°ü±¤Áö
+	// ê´€ê´‘ì§€
 
-	static_cast<SubjectBlock*>boards[29] = new SubjectBlock;
-	static_cast<SubjectBlock*>boards[30] = new SubjectBlock;
-	static_cast<SubjectBlock*>boards[31] = new SubjectBlock;
+	boards[29] = new SubjectBlock;
+	boards[30] = new SubjectBlock;
+	boards[31] = new SubjectBlock;
 	
 	boards[32] = new EventBlock;
 
-	static_cast<SubjectBlock*>boards[33] = new SubjectBlock;
-	static_cast<SubjectBlock*>boards[34] = new SubjectBlock;
-	static_cast<SubjectBlock*>boards[35] = new SubjectBlock;
+	boards[33] = new SubjectBlock;
+	boards[34] = new SubjectBlock;
+	boards[35] = new SubjectBlock;
 	
 
 }
