39979819057daf68a13fadd7ca1a3ea70d7dd1d2
Wonpyo-Park <parkwonpyo@postech.ac.kr>
2013-11-29 04:51:47 +0900

    commit

--- src/client/subjectblock.h
+++ src/client/subjectblock.h
@@ -20,10 +20,6 @@ public:
     virtual ~SubjectBlock();
     SubjectType::Type getType() const;
     virtual void enter(Player* player);
-<<<<<<< HEAD
-=======
-    void setSubjectBlock(SubjectType::Type blockdepartment, std::string subjectname);
     void decideGrade();
 
->>>>>>> d5d57e3563fae4bb0820c19a94ac2b73049c8650
 };
