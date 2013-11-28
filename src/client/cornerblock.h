721f2ecf6d6ed40ceaff810791f979270f615139
Bums Kim <bskim45@gmail.com>
2013-11-28 19:40:01 +0900

    solved marge confilct

--- src/client/cornerblock.h
+++ src/client/cornerblock.h
@@ -1,13 +1,9 @@
 #pragma once
 #include "player.h"
 #include "block.h"
-<<<<<<< HEAD
 #include <ctime>
 #include <cstdlib>
-
-=======
 #include "types.h"
->>>>>>> b6f959267c62efc4f3dbef89a64eb7581955c3ce
 
 class CornerBlock : public Block
 {
