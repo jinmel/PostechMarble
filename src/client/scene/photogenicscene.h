d8fabe2595477762fbf857e058b4322e552372d7
chanmi <chanmi0830@gmail.com>
2013-12-04 04:18:51 +0900

    photogenic added

---
+++ src/client/scene/photogenicscene.h
@@ -0,0 +1,16 @@
+#ifndef PHOTOGENICSCENE_H
+#define PHOTOGENICSCENE_H
+
+class Photogenicscene : public QgraphicsScene
+{
+    Q_OBJECT
+public:
+    explicit Photogenicscene(QObject *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // PHOTOGENICSCENE_H
