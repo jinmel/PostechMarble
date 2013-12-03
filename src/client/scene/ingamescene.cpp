bb0ba39adf55bd70dd225bd90ed449c1f6ac4715
HamG <tedkhg@gmail.com>
2013-12-04 06:30:47 +0900

    ingamescene : renew backgrounds

--- src/client/scene/ingamescene.cpp
+++ src/client/scene/ingamescene.cpp
@@ -15,7 +15,7 @@ IngameScene::IngameScene(qreal x, qreal y,
 {
     Q_CHECK_PTR(window);
 
-    setBackgroundPixmap(":/images/ingame/board/board_back.png");
+    setBackgroundPixmap(":/images/ingame/board/background.png");
     LocalGame * game = LocalGame::getInst();
 
     board = new Board(this,window);
@@ -69,10 +69,12 @@ IngameScene::IngameScene(qreal x, qreal y,
     status1 = new QGameItem(this, window);
     status1->setImage(":images/ingame/status/status1.png");
     status1->setPos(350, 160);
+    status1->setZValue(2);
 
     status2 = new QGameItem(this, window);
     status2->setImage(":images/ingame/status/status2.png");
     status2->setPos(660, 160);
+    status2->setZValue(2);
 
     //Signal / Slots connection
     Dice * dice = Dice::getInst();
