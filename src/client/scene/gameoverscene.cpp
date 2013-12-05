2e02a11b94f7cd4275bfd2e7db8191b39c3d2c4d
chanmi <chanmi0830@gmail.com>
2013-12-06 08:11:58 +0900

    gameover

---
+++ src/client/scene/gameoverscene.cpp
@@ -0,0 +1,29 @@
+#include "gameoverscene.h"
+#include "../types.h"
+#include <QGraphicsItem>
+#include <QDebug>
+
+GameoverScene::GameoverScene(qreal x, qreal y,
+                             qreal width, qreal height,
+                             QObject *parent)
+    : QGraphicsScene(x, y, width, height, parent)
+{
+    this->window = dynamic_cast<MainWindow*> (parent);
+    if(this->window == NULL) qDebug()<<"window is null!";
+    Q_CHECK_PTR(this->window);
+
+    setupGameover();
+}
+
+GameoverScene::~GameoverScene()
+{
+    delete ready_button;
+}
+
+/*
+
+
+
+
+
+*/
